926e6545dd35136b24bf19229d3a9b433c5caa96
Keder <kedernath.mallick.tint022@gmail.com>
2022-03-22 13:35:58 +0530

    static path added

--- EmailSend/urls.py
+++ EmailSend/urls.py
@@ -1,9 +1,10 @@
 from unicodedata import name
 from .views import *
 from django.urls import path 
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('send_email',send_email_user,name="send_email_user"),
     path('check',check)
-] 
+] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
