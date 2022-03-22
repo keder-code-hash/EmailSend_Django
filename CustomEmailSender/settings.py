d817e8af36ca7e0bc683cdfdbea6471e7fc5d9dd
Keder <kedernath.mallick.tint022@gmail.com>
2022-03-22 12:40:18 +0530

    paths fixed

--- CustomEmailSender/settings.py
+++ CustomEmailSender/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'CustomEmailSender.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR /'templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'CustomEmailSender.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
